dbd842417ca5116014bc8ce608084b4f340a6be6
John Singh <johnjaskaran.singh@nvenergy.com>
2023-10-06 16:20:44 -0700

    Add fighter model

---
+++ lib/src/api_utils/fighter/fighter_model.dart
@@ -0,0 +1,19 @@
+class Fighter {
+  final String name;
+  final int health;
+  final String id;
+
+  Fighter({
+    required this.name,
+    required this.health,
+    required this.id,
+  });
+
+  factory Fighter.fromJson(Map<String, dynamic> json) {
+    return Fighter(
+      name: json['name'],
+      health: json['health'],
+      id: json['id'],
+    );
+  }
+}
