e357f4f8d48848cd3f8043456f51b3da8c1fd3ef
John Singh <johnjaskaran.singh@nvenergy.com>
2023-10-13 11:11:34 -0700

    Update test file

--- test/unit/arena_selection/arena_selection_utils_test.dart
+++ test/unit/arena_selection/arena_selection_utils_test.dart
@@ -1,18 +1,21 @@
+import 'package:game_template/src/arena_selection/utils/api_arena_selection_utils.dart';
 import 'package:game_template/src/arena_selection/utils/in_memory_arena_selection_utils.dart';
+import 'package:game_template/src/play_session/models/arena.dart';
 import 'package:test/test.dart';
 
 void main() {
-  final arenaUtils = InMemoryArenaSelection();
+  final arenaUtils = ApiArenaSelection();
   group('getOpenArenas', () {
-    test('returns a list of arenas', () async {
+    test('returns an open arena', () async {
       // Arrange
+      const fighterId = "1";
 
       // Act
-      final result = await arenaUtils.getOpenArenas();
+      final result = await arenaUtils.joinOpenArena(fighterId);
 
       // Assert
-      expect(result, isA<List>());
-      expect(result, isNotEmpty);
+      expect(result, isA<Arena>());
+      expect(result.fighters.length, 1);
     });
   });
 }
